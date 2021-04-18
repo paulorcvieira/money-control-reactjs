8e0a34236c6804c53038190b2f23a3002af94289
Paulo Vieira <paullocostta86@gmail.com>
2021-04-17 21:20:29 -0300

    feat: create miragejs connection and routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer, Model } from 'miragejs'
+
 import App from './App';
+
+createServer({
+  models: {
+    transaction: Model
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Freelance de website",
+          type: "deposit",
+          category: "Dev",
+          amount: 6000,
+          createdAt: new Date("2021-02-16 09:00:00"),
+        },
+        {
+          id: 2,
+          title: 'Gasolina',
+          type: 'withdraw',
+          category: 'Carro',
+          amount: 800,
+          createdAt: new Date("2021-04-17 10:10:00"),
+        },
+      ]
+    });
+  },
+
+  routes() {
+    this.namespace = 'api'
+
+    this.get('/transactions', () => {
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data)
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>

68e8db0937cdd401e7a3b4f1a86a1c498d3f404e
Paulo Vieira <paullocostta86@gmail.com>
2021-04-17 21:24:32 -0300

    refactor: add header props

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,12 +2,17 @@ import logo from '../../assets/logo.svg'
 
 import * as S from './styles'
 
-export const Header = () => {
+type HeaderProps = {
+  onOpenNewTransaction: () => void
+}
+
+export const Header = ({ onOpenNewTransaction }: HeaderProps) => {
+
   return (
     <S.Container>
       <S.Content>
         <img src={logo} alt="money control"/>
-        <button type="button">
+        <button type="button" onClick={() => onOpenNewTransaction()}>
           Nova transação
         </button>
       </S.Content>

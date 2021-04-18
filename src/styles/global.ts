aecc96d6e44e11aba2485bcc2d9d66ecb0f1c778
Paulo Vieira <paullocostta86@gmail.com>
2021-04-17 21:22:23 -0300

    feat: create new styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -11,6 +11,12 @@ export const GlobalStyle = createGlobalStyle`
 
     --blue-light: #6933ff;
 
+    --modal-background: #f0f2f5;
+    --modal-input-background: #e7e9ee;
+    --modal-input-text: #9696b3;
+    --modal-input-border: #d7d7d7;
+    --modal-overlay: rgba(0, 0, 0, 0.5);
+
     --text-title: #363f5f;
     --text-body: #9696b3;
     --text-white: #ffffff;
@@ -53,5 +59,42 @@ export const GlobalStyle = createGlobalStyle`
   [disabled] {
     opacity: 0.6;
     cursor: not-allowed;
+  }
+
+  .react-modal-overlay {
+    background: var(--modal-overlay);
+
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .react-modal-content {
+    width: 100%;
+    max-width: 33.17rem;
+    background: var(--modal-background);
+    padding: 3rem;
+    position: relative;
+    border-radius: 0.25rem;
+  }
+
+  .react-modal-close {
+    position: absolute;
+    right: 1.5rem;
+    top: 1.5rem;
+    border: 0;
+    background: transparent;
+
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.85);
+    }
   }
 `

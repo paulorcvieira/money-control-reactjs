b89029b2ed8a32af14d610c673abfe0c7e4d416e
Paulo Vieira <paullocostta86@gmail.com>
2021-04-17 21:29:50 -0300

    feat: create modal functions, header props and transactions modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
+import { useCallback, useState } from 'react'
+
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
+import { NewTransactionModal } from './components/NewTransactionModal';
+
 import { GlobalStyle } from './styles/global';
 
 function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
+
+  const handleOpenNewTransaction = useCallback(() => {
+    setIsNewTransactionModalOpen(true)
+  }, [])
+
+  const handleCloseNewTransaction = useCallback(() => {
+    setIsNewTransactionModalOpen(false)
+  }, [])
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransaction={handleOpenNewTransaction} />
       <Dashboard />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransaction}
+      />
       <GlobalStyle />
     </>
   );
